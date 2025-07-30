a0336fb511366eb68d12260077e6247f3e2cff58
JT45A3AS <JT45A3AS@KHLC01202.ad.jpd.co.jp>
2025-07-30 11:54:51 +0900

    自動テスト実装

--- hrms2025_test/src/main/java/scenario/SampleTest.java
+++ hrms2025_test/src/main/java/scenario/SampleTest.java
@@ -5,81 +5,78 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import utility.CommonUtil;
 
 public class SampleTest extends TestBase {
 
-	/**
-	 * Chromeでアプリを開く
-	 */
-	@BeforeMethod(alwaysRun = true)
-	public void before() {
-		driver = new ChromeDriver();
-		driver.get(SHINABLE_CLOUD_PATH);
-		driver.manage().window().maximize();
-	}
+  /**
+   * Chromeでアプリを開く
+   */
+  @BeforeMethod(alwaysRun = true)
+  public void before() {
+    driver = new ChromeDriver();
+    driver.get(SHINABLE_CLOUD_PATH);
+    driver.manage().window().maximize();
+  }
 
-	/**
-	 * 各テスト後の処理
-	 * テストがfailした場合、スクショをとる
-	 * @param result
-	 */
-	@AfterMethod(alwaysRun = true)
-	public void after(ITestResult result) {
-		//failしたらスクショ
-		if (result.getStatus() != ITestResult.SUCCESS) {
-			CommonUtil.takeScreenShot(driver, ".\\result", result.getMethod().getMethodName());
-		}
-		//ブラウザを閉じる
-		driver.quit();
-	}
+  /**
+   * 各テスト後の処理 テストがfailした場合、スクショをとる
+   * 
+   * @param result
+   */
+  @AfterMethod(alwaysRun = true)
+  public void after(ITestResult result) {
+    // failしたらスクショ
+    if (result.getStatus() != ITestResult.SUCCESS) {
+      CommonUtil.takeScreenShot(driver, ".\\result", result.getMethod().getMethodName());
+    }
+    // ブラウザを閉じる
+    driver.quit();
+  }
 
-	/**
-	 * 1.ログイン
-	 * 2.必須項目情報のみで社員登録
-	 * 3.登録した社員を削除
-	 * 4.ログアウト
-	 */
-	@Test(groups = "sample")
-	public void sampleTest001() {
-		//1.ログイン
-		login("admin@admin.com", "admin");
-		// ホーム画面が開く
-		assertPageTitle("ホーム");
+  /**
+   * 1.ログイン 2.必須項目情報のみで社員登録 3.登録した社員を削除 4.ログアウト
+   */
+  @Test(groups = "scenario")
+  public void sampleTest001() {
+    // 1.ログイン
+    login("admin@jpd.co.jp", "test");
+    // ホーム画面が開く
+    // assertPageTitle("ホーム");
 
-		//2.必須項目情報のみで社員登録
-		// 新規登録ボタンを押す
-		click("//*[text()='従業員情報検索']");
-		// ページ確認
-		assertPageTitle("従業員情報検索");
+    // 2.必須項目情報のみで社員登録
+    // 従業員情報一覧画面を開く
+    click("/html/body/div[2]/div[1]/div/div/form[3]/button");
+    // ページ確認
+    assertPageTitle("従業員情報一覧");
 
-		// 新規登録画面を開く
-		click("//*[text()='新規登録']");
-		// ページ確認
-		assertPageTitle("従業員情報登録");
-		
-		//社員情報入力
-		final String employeeNum = "0001";
-		final String mailaddress = "Purota.NIHON@jpd.co.jp";
-		inputEmployeeInfo(employeeNum, mailaddress);
-		// 登録
-		click("//*[@id=\"upload\"]");
-		//メッセージバー確認
-		assertMessageBar("従業員情報の登録が完了しました。");
-		assertPageTitleDisplayedMessageBar("従業員情報詳細");
+    // 新規登録ボタンを押す
+    click("/html/body/div[2]/div[3]/form/input");
+    // ページ確認
+    // assertPageTitle("新規従業員登録");
 
-		//3.登録した社員を削除
-		click("/html/body/div[2]/div[1]/button");
-		assertText("/html/body/div[2]/div[2]/div[1]/p[1]", "以下の従業員情報を削除してもよろしいでしょうか");
-		assertText("/html/body/div[2]/div[2]/div[1]/p[2]", "・" + "日本プロ太");
-		click("//*[@id=\"modalExecute\"]");
-		//メッセージバー確認
-		assertMessageBar("従業員情報の削除が完了しました。");
-		assertPageTitle("従業員情報検索");
+    // 社員情報入力
+    final String employeeNo = "3333";
+    final String mailAddress = "Purota.NIHON@jpd.co.jp";
+    inputEmployeeInfo(employeeNo, mailAddress);
 
-		//4.ログアウト
-		click("/html/body/header/nav/div/ul/li[4]/a");
-		click("//*[@id=\"glovalNaviLoginDropMenu2\"]/li[2]/form");
-	}
+    // 登録
+    click("/html/body/div[2]/form/div[1]/input");
+    // メッセージバー確認
+    assertMessageBar("従業員情報の登録が完了しました。");
+    // assertPageTitleDisplayedMessageBar("従業員情報詳細");
+
+    // 3.登録した社員を削除
+    click("/html/body/div[2]/div[1]/div/button");
+    assertText("//*[@id=\"deleteModal\"]/div/div/div[2]/p[1]", "以下の従業員情報を削除してもよろしいでしょうか。");
+    // // assertText("/html/body/div[2]/div[2]/div[1]/p[2]", "・" + "日本プロ太");
+    click("//*[@id=\"deleteModal\"]/div/div/div[3]/form/button[1]");
+    // メッセージバー確認
+    assertMessageBar("従業員情報の削除が完了しました。");
+    // assertPageTitle("従業員情報一覧");
+
+    // 4.ログアウト
+    click("/html/body/div[1]/div/div/div[2]/div/button");
+    click("//*[@id=\"menu\"]/ul/form[2]/button");
+  }
 }
--- hrms2025_test/src/main/java/scenario/TestBase.java
+++ hrms2025_test/src/main/java/scenario/TestBase.java
@@ -1,145 +1,190 @@
 package scenario;
 
 import static org.testng.Assert.*;
-
 import java.time.Duration;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBase {
 
-	WebDriver driver;
-	Duration waitTime;
-	WebDriverWait wait;
+  WebDriver driver;
+  Duration waitTime;
+  WebDriverWait wait;
 
-	// アプリのパス
-	static final String SHINABLE_CLOUD_PATH = "https://shinable.azurewebsites.net/login";
+  // アプリのパス
+  static final String SHINABLE_CLOUD_PATH = "http://localhost:8081/hrms2025/login?logout";
 
-	/**
-	 * 入力
-	 * @param xpath
-	 * @param data
-	 */
-	public void input(String xpath, String data) {
-		waitForElementVisible(xpath);
-		driver.findElement(By.xpath(xpath)).sendKeys(data);
-	}
+  /**
+   * 入力
+   * 
+   * @param xpath
+   * @param data
+   */
+  public void input(String xpath, String data) {
+    waitForElementVisible(xpath);
+    driver.findElement(By.xpath(xpath)).sendKeys(data);
+  }
 
-	/**
-	 * クリック
-	 * @param xpath
-	 */
-	public void click(String xpath) {
-		waitForElementVisible(xpath);
-		driver.findElement(By.xpath(xpath)).click();
-	}
+  /**
+   * クリック
+   * 
+   * @param xpath
+   */
+  public void click(String xpath) {
+    waitForElementVisible(xpath);
+    WebElement element = driver.findElement(By.xpath(xpath));
 
-	/**
-	 * 表示待ち
-	 * @param xpath
-	 */
-	public void waitForElementVisible(String xpath) {
-		waitTime = Duration.ofSeconds(20);
-		wait = new WebDriverWait(driver, waitTime);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
-	}
+    try {
+      element.click(); // 通常のクリック
+    } catch (Exception e) {
+      // JavaScriptで強制クリック
+      JavascriptExecutor js = (JavascriptExecutor) driver;
+      js.executeScript("arguments[0].click();", element);
+    }
 
-	/**
-	 * テキストの表示待ち
-	 * @param xpath
-	 */
-	public void waitForElementTextChange(String xpath, String text) {
-		waitTime = Duration.ofSeconds(20);
-		wait = new WebDriverWait(driver, waitTime);
-		wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(By.xpath(xpath)), text));
-	}
+  }
 
-	/**
-	 * テキストの確認
-	 * @param xpath
-	 * @param name
-	 */
-	public void assertText(String xpath, String text) {
-		waitForElementVisible(xpath);
-		assertEquals(driver.findElement(By.xpath(xpath)).getText(), text);
-	}
+  // public void click(String xpath) {
+  // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+  // WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+  // element.click();
+  // }
 
-	/**
-	 * ログイン
-	 * @param mailAddress
-	 * @param password
-	 */
-	public void login(String mailAddress, String password) {
-		final String MAILADDRESS_INPUT_XPATH = "//*[@id=\"mailAddress\"]";
-		final String PASSWORD_INPUT_XPATH = "//*[@id=\"password\"]";
-		final String LOGIN_BUTTON_XPATH = "//button[text()='ログイン']";
-		// input mailaddress
-		input(MAILADDRESS_INPUT_XPATH, mailAddress);
-		// input password
-		input(PASSWORD_INPUT_XPATH, password);
-		// click login button
-		click(LOGIN_BUTTON_XPATH);
-	}
 
-	/**
-	 * 社員情報を入力
-	 * @param employeeNum
-	 * @param mailaddress
-	 */
-	public void inputEmployeeInfo(String employeeNum, String mailaddress) {
-		// input employee num
-		input("//*[@id=\"number\"]", employeeNum);
-		// input name
-		input("//*[@id=\"fullName\"]", "日本プロ太");
-		// input mailaddress
-		input("//*[@id=\"mailAddress\"]", mailaddress);
-		// select rank
-		click("/html/body/div[2]/div/form/div[8]/div/div/input");
-		click("//*[@class=\"dropdown-content select-dropdown\"]/li[2]");
-		// select group
-		click("//*[@id=\"open\"]");
-		// dialog
-		click("//*[@id=\"node0\"]");
-		click("//*[@id=\"select\"]");
-		//入力出来ていない時があったのでリトライを入れる
-		if (driver.findElement(By.xpath("//*[@id=\"organizationName\"]")).getText().equals(null)) {
-			// select group
-			click("//*[@id=\"open\"]");
-			//dialog
-			click("//*[@id=\"node0\"]");
-			click("//*[@id=\"select\"]");
-		}
-		//input password
-		input("//*[@id=\"password\"]", "P@ssw0rd");
-	}
+  /**
+   * 表示待ち
+   * 
+   * @param xpath
+   */
+  public void waitForElementVisible(String xpath) {
+    waitTime = Duration.ofSeconds(20);
+    wait = new WebDriverWait(driver, waitTime);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+  }
 
-	/**
-	 * ページタイトルの確認
-	 * @param page title
-	 */
-	public void assertPageTitle(String title) {
-		waitForElementTextChange("/html/body/div[2]/h1", title);
-		ExpectedConditions.titleIs(title);
-	}
+  /**
+   * テキストの表示待ち
+   * 
+   * @param xpath
+   */
+  public void waitForElementTextChange(String xpath, String text) {
+    waitTime = Duration.ofSeconds(20);
+    wait = new WebDriverWait(driver, waitTime);
+    wait.until(
+        ExpectedConditions.textToBePresentInElement(driver.findElement(By.xpath(xpath)), text));
+  }
 
-	/**
-	 * ページタイトルの確認（メッセージバー表示時）
-	 * @param page title
-	 */
-	public void assertPageTitleDisplayedMessageBar(String title) {
-		waitForElementTextChange("/html/body/div[2]/div[1]/h1", title);
-		ExpectedConditions.titleIs(title);
-	}
 
-	/**
-	 * メッセージバーの確認
-	 * @param message
-	 */
-	public void assertMessageBar(String message) {
-		waitForElementVisible("//*[@id=\"messageBar\"]");
-		assertText("//*[@id=\"messageBar\"]/span", message);
-	}
+  public void inputDate(String xpath, String dateValue) {
+    waitForElementVisible(xpath);
+    WebElement element = driver.findElement(By.xpath(xpath));
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    js.executeScript("arguments[0].value = arguments[1];", element, dateValue);
+  }
+
+
+  /**
+   * テキストの確認
+   * 
+   * @param xpath
+   * @param name
+   */
+  public void assertText(String xpath, String text) {
+    waitForElementVisible(xpath);
+    assertEquals(driver.findElement(By.xpath(xpath)).getText(), text);
+  }
+
+  /**
+   * ログイン
+   * 
+   * @param mailAddress
+   * @param password
+   */
+  public void login(String mailAddress, String password) {
+    final String MAILADDRESS_INPUT_XPATH = "//*[@id=\"usernameInput\"]";
+    final String PASSWORD_INPUT_XPATH = "//*[@id=\"passwordInput\"]";
+    final String LOGIN_BUTTON_XPATH = "/html/body/div[2]/div/form/div[3]/input";
+    // input mailaddress
+    input(MAILADDRESS_INPUT_XPATH, mailAddress);
+    // input password
+    input(PASSWORD_INPUT_XPATH, password);
+    // click login button
+    click(LOGIN_BUTTON_XPATH);
+  }
+
+  /**
+   * 社員情報を入力
+   * 
+   * @param employeeNum
+   * @param mailaddress
+   */
+  public void inputEmployeeInfo(String employeeNo, String mailAddress) {
+    // input employee num
+    input("//*[@id=\"employeeNo\"]", employeeNo);
+    // input name
+    input("//*[@id=\"fullName\"]", "日本プロ太");
+    // input mailaddress
+    input("//*[@id=\"mailAddress\"]", mailAddress);
+    // select gender (男)
+    click("//*[@id=\"gender1\"]");
+    // input hiredate
+    inputDate("//*[@id=\"hireDate\"]", "2025-03-29");
+    // input birthdate
+    inputDate("//*[@id=\"birthDate\"]", "2000-07-29");
+    // select rank
+    click("//*[@id=\"employeeRank\"]");
+    click("//*[@id=\"employeeRank\"]/div[1]/option");
+    // select organization
+    click("/html/body/div[2]/form/table/tbody/tr[9]/td/button");
+    // dialog
+    click("//*[@id=\"organizationModal\"]/div/div/div[2]/div/li[3]/button");
+    click("//*[@id=\"de1c657b-6ab9-412b-8137-32d0496ad58a\"]/li[3]/button");
+    click("//*[@id=\"0b9ff3de-5317-4944-9921-d77d23151ce3\"]/li/span");
+    // // //入力出来ていない時があったのでリトライを入れる
+    // // if (driver.findElement(By.xpath("//*[@id=\"organizationName\"]")).getText().equals(null))
+    // {
+    // // // select group
+    // // click("//*[@id=\"open\"]");
+    // // //dialog
+    // // click("//*[@id=\"node0\"]");
+    // // click("//*[@id=\"select\"]");
+    // // }
+    // input password
+    input("//*[@id=\"password\"]", "test");
+    input("//*[@id=\"confirmPassword\"]", "test");
+  }
+
+  /**
+   * ページタイトルの確認
+   * 
+   * @param page title
+   */
+  public void assertPageTitle(String title) {
+    waitForElementTextChange("/html/body/div[2]/div[1]/h2", title);
+    ExpectedConditions.titleIs(title);
+  }
+
+  /**
+   * ページタイトルの確認（メッセージバー表示時）
+   * 
+   * @param page title
+   */
+  public void assertPageTitleDisplayedMessageBar(String title) {
+    waitForElementTextChange("/html/body/div[2]/div[1]/h1", title);
+    ExpectedConditions.titleIs(title);
+  }
+
+  /**
+   * メッセージバーの確認
+   * 
+   * @param message
+   */
+  public void assertMessageBar(String message) {
+    waitForElementVisible("/html/body/div[1]/div/div/div[3]");
+    assertText("/html/body/div[1]/div/div/div[3]/span", message);
+  }
 }
